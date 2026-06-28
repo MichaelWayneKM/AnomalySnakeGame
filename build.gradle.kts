a297bcd870906c94ffd9e38ad5d96ae50482bb21
MichaelWayneKM <kromwell005@gmail.com>
2026-06-28 04:27:39 +0300

    core game wfl patch

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,12 @@ jlink {
     }
 }
 
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+    }
+}
+
 tasks.test {
     useJUnitPlatform()
 }
